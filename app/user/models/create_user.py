3d1af76f05adf854b6e19d5b819b65667cae2584
harshhirpara01 <harshhirpara0001@gmail.com>
2026-04-02 01:14:16 +0530

    user datamodel and schemas done

---
+++ app/user/models/create_user.py
@@ -0,0 +1,23 @@
+from operator import index
+
+from sqlalchemy import Column,Integer,String,Boolean,DateTime
+from sqlalchemy.sql import func
+from shared.db import Base
+
+
+
+class Create_User(Base):
+    __tablename__ = "users"
+
+    id  = Column(Integer,primary_key = True,index = True),
+    full_name = Column(String(100), nullable = False)
+    email = Column(String(120), unique = True, nullable = False, index = True)
+    password_hash = Column(String(255), nullable = False)
+    role = Column(String(20), nullable=False, default = "viewer")
+    country = Column(String(100), nullable = True)
+    is_active = Column(Boolean, default = True, nullable = False)
+    is_blocked = Column(Boolean, default  = False, nullable = False)
+    is_deleted = Column(Boolean, default = False, nullable = False)
+    created_at = Column(DateTime(timezone= True), server_default = func.now())
+    updated_at = Column(DateTime(timezone =True), server_default =func.now(), onupdate =func.now())
+    deleted_at = Column(DateTime(timezone= True), nullable=True)
---
+++ app/user/schemas/user_schema.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Literal, Optional
+
+
+class CreateUserSchema(BaseModel):
+    full_name: str = Field(..., min_length=2, max_length=100)
+    email: EmailStr
+    password: str = Field(..., min_length=6, max_length=50)
+    role: Literal["viewer", "analyst", "admin"] = "viewer"
+    country: Optional[str] = None
+    is_active: bool = True
+    is_blocked: bool = False
+
+
+class UserResponseSchema(BaseModel):
+    id: int
+    full_name: str
+    email: EmailStr
+    role: str
+    country: Optional[str]
+    is_active: bool
+    is_blocked: bool
+    is_deleted: bool
+
+    class Config:
+        from_attributes = True
